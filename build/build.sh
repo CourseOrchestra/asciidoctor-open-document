36b0fa74483787ad594e3ff127b4a6f51f35cb22
fiddlededee <consulting@yandex.ru>
2021-05-08 01:16:56 +0300

    GA15

--- build/build.sh
+++ build/build.sh
@@ -1,29 +1,45 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-cd ..
-
-rm target -rf
-mkdir target
-mkdir target/out
-cp test/test_cases/stew target -r
-
-echo build image
-build/build_image.sh
-
-echo build README.adoc
-docker run --rm -v $(pwd):/documents/ -w /documents/ asciidoctor-od build/build_readme.sh
-docker run --rm -v $(pwd):/documents/ -w /documents/ asciidoctor-od asciidoctor README.adoc -o target/out/index.html
-
-echo make fodt
-docker run --rm -v $(pwd):/documents/ -w /documents/target/stew asciidoctor-od a-od-pre -r asciidoctor-mathematical -r asciidoctor-diagram test.adoc -o pre.xml
-docker run --rm -v $(pwd):/documents/ -w /documents/target/stew asciidoctor-od a-od-out -c /usr/local/a-od/a-od-my/my-cp-example.rb -i pre.xml -o test.fodt
+##!/bin/bash
+#cd "$(dirname "$0")"
+#cd ..
+#
+#rm target -rf
+#mkdir target
+#mkdir target/out
+#cp test/test_cases/stew target -r
+#
+#echo build image
+#build/build_image.sh
+#
+#echo build README.adoc
+#docker run --rm -v $(pwd):/documents/ -w /documents/ asciidoctor-od build/build_readme.sh
+#docker run --rm -v $(pwd):/documents/ -w /documents/ asciidoctor-od asciidoctor README.adoc -o target/out/index.html
+#
+#echo make fodt
+#docker run --rm -v $(pwd):/documents/ -w /documents/target/stew asciidoctor-od a-od-pre -r asciidoctor-mathematical -r asciidoctor-diagram test.adoc -o pre.xml
+#docker run --rm -v $(pwd):/documents/ -w /documents/target/stew asciidoctor-od a-od-out -c /usr/local/a-od/a-od-my/my-cp-example.rb -i pre.xml -o test.fodt
 
 echo convert to pdf 
 #cp build/libre-office/.config target -r
 #docker run --rm -v $(pwd)/target:/home/alpine woahbase/alpine-libreoffice:x86_64 env 
-docker run -e "HOME=/home/alpine" --rm -v $(pwd)/target:/home/alpine woahbase/alpine-libreoffice:x86_64 soffice --headless "macro:///Standard.Module1.toPdf(/home/alpine/stew/test.fodt)"
+#docker run -e "HOME=/home/alpine" --rm -v $(pwd)/target:/home/alpine woahbase/alpine-libreoffice:x86_64 soffice --headless "macro:///Standard.Module1.toPdf(/home/alpine/stew/test.fodt)"
+#cp target/stew/test.pdf target/out
+#cp target/stew/test.fodt target/out
+
+docker run -d --name libreoffice -p 8100:8100 hdejager/libreoffice-api
+sleep 3
+docker cp target/stew/test.fodt libreoffice:/tmp/test.fodt
+docker exec -i libreoffice unoconv --connection 'socket,host=127.0.0.1,port=8100,tcpNoDelay=1;urp;StarOffice.ComponentContext' -f pdf /tmp/test.fodt
+docker exec -i libreoffice unoconv --connection 'socket,host=127.0.0.1,port=8100,tcpNoDelay=1;urp;StarOffice.ComponentContext' -f odt /tmp/test.fodt
+docker exec -i libreoffice unoconv --connection 'socket,host=127.0.0.1,port=8100,tcpNoDelay=1;urp;StarOffice.ComponentContext' -f docx /tmp/test.fodt
+docker cp libreoffice:/tmp/test.odt target/stew
+docker cp libreoffice:/tmp/test.pdf target/stew
+docker cp libreoffice:/tmp/test.docx target/stew
+docker kill libreoffice
+docker rm libreoffice
 cp target/stew/test.pdf target/out
 cp target/stew/test.fodt target/out
+cp target/stew/test.odt target/out
+cp target/stew/test.docx target/out
 
 echo test a-od producer
 docker run --rm -v $(pwd):/documents/ asciidoctor-od ruby test/test.rb  | tee target/unit_test.log
