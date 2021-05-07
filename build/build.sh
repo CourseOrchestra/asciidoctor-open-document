80b9512beeafa8cc71aa4e0fe8fa504f56f06640
fiddlededee <consulting@yandex.ru>
2021-05-07 20:58:39 +0300

    GA5

--- build/build.sh
+++ build/build.sh
@@ -13,25 +13,25 @@ ls
 # build image
 build/build_image.sh
 
-docker run -v --rm $(pwd):/documents/ -w /documents/ asciidoctor-od echo $PATH
+docker run --rm -v $(pwd):/documents/ -w /documents/ asciidoctor-od bash -c 'echo $PATH'
 
 # build README.adoc
 build/build_readme.sh
-docker run -v --rm $(pwd):/documents/ -w /documents/ asciidoctor-od asciidoctor README.adoc -o target/out/index.html
+docker run --rm -v $(pwd):/documents/ -w /documents/ asciidoctor-od asciidoctor README.adoc -o target/out/index.html
 
 # make fodt
-docker run -v --rm $(pwd):/documents/ -w /documents/target/stew asciidoctor-od a-od-pre -r asciidoctor-mathematical -r asciidoctor-diagram test.adoc -o pre.xml
-docker run -v --rm $(pwd):/documents/ -w /documents/target/stew asciidoctor-od a-od-out -c /usr/local/a-od/a-od-my/my-cp-example.rb -i pre.xml -o test.fodt
-
-# convert to pdf 
-cp build/libre-office/.config target -r
-docker run -v --rm $(pwd)/target:/home/alpine woahbase/alpine-libreoffice:x86_64 soffice --headless "macro:///Standard.Module1.toPdf(/home/alpine/stew/test.fodt)"
-cp target/stew/test.pdf target/out
-cp target/stew/test.fodt target/out
-
-#test a-od producer
-docker run -v --rm $(pwd):/documents/ asciidoctor-od ruby test/test.rb  | tee target/unit_test.log
-
-#test final fodt
-docker run -v --rm $(pwd):/documents/ asciidoctor-od ruby test/test_fodt.rb  | tee target/result_test.log
-
+#docker run --rm -v $(pwd):/documents/ -w /documents/target/stew asciidoctor-od a-od-pre -r asciidoctor-mathematical -r asciidoctor-diagram test.adoc -o pre.xml
+#docker run --rm -v $(pwd):/documents/ -w /documents/target/stew asciidoctor-od a-od-out -c /usr/local/a-od/a-od-my/my-cp-example.rb -i pre.xml -o test.fodt
+#
+## convert to pdf 
+#cp build/libre-office/.config target -r
+#docker run --rm -v $(pwd)/target:/home/alpine woahbase/alpine-libreoffice:x86_64 soffice --headless "macro:///Standard.Module1.toPdf(/home/alpine/stew/test.fodt)"
+#cp target/stew/test.pdf target/out
+#cp target/stew/test.fodt target/out
+#
+##test a-od producer
+#docker run --rm -v $(pwd):/documents/ asciidoctor-od ruby test/test.rb  | tee target/unit_test.log
+#
+##test final fodt
+#docker run --rm -v $(pwd):/documents/ asciidoctor-od ruby test/test_fodt.rb  | tee target/result_test.log
+#
