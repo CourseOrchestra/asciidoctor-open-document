fc543da41f577587945f0078bd391c161e95683c
fiddlededee <consulting@yandex.ru>
2021-05-07 16:23:26 +0300

    Deploy to github pages

--- build/build.sh
+++ build/build.sh
@@ -8,6 +8,8 @@ cp test/test_cases/stew target -r
 
 # build README.adoc
 build/build_readme.sh
+mkdir target/out
+docker run -it -v $(pwd):/documents/ -w /documents/ asciidoctor-od asciidoctor README.adoc -o target/out/index.html
 
 # build image
 build/build_image.sh
@@ -19,6 +21,8 @@ docker run -it -v $(pwd):/documents/ -w /documents/target/stew asciidoctor-od a-
 # convert to pdf 
 cp build/libre-office/.config target -r
 docker run --rm -v $(pwd)/target:/home/alpine woahbase/alpine-libreoffice:x86_64 soffice --headless "macro:///Standard.Module1.toPdf(/home/alpine/stew/test.fodt)"
+cp target/stew/test.pdf target/out
+cp target/stew/test.fodt target/out
 
 #test a-od producer
 docker run -it -v $(pwd):/documents/ asciidoctor-od ruby test/test.rb  | tee target/unit_test.log
