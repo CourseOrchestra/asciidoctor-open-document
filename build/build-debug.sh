dd8cf2eb7bcfc9b2975d2489374dbcc72fe97cbb
fiddlededee <new@mail.com>
2022-11-06 19:06:26 +0300

    ci: updated debug template

--- build/build-debug.sh
+++ build/build-debug.sh
@@ -8,15 +8,17 @@ mkdir target/out
 cp test/test_cases/stew target -r
 cp test/test_cases/asciidoctor-pdf target -r
 
-#cd target/stew
-#asciidoctor -b fodt -T ../../lib/slim -r asciidoctor-mathematical -r asciidoctor-diagram test.adoc -o pre.xml
+cd target/stew
+#ruby ../../test/test.rb
+asciidoctor -b fodt -T ../../lib/slim -r asciidoctor-mathematical -r asciidoctor-diagram test.adoc -o pre.xml
 #ruby ../../lib/a-od-producer/asciidoctor-od.rb -c ../../lib/a-od-my/my-cp-example.rb -i pre.xml -o test.fodt
-#docker run --rm -v $(pwd):/documents/ curs/asciidoctor-od unoconv -f pdf test.fodt
-#cd ../..
-
-cd target/asciidoctor-pdf
-asciidoctor -b fodt -T ../../lib/slim -r asciidoctor-mathematical -r asciidoctor-diagram chronicles-example.adoc -o pre.xml
-ruby ../../lib/a-od-producer/asciidoctor-od.rb  -i pre.xml -f chronicles-template.fodt -o chronicles-example.fodt
-docker run --rm -v $(pwd):/documents/ curs/asciidoctor-od unoconv -f pdf chronicles-example.fodt
+ruby ../../lib/a-od-producer/asciidoctor-od.rb -i pre.xml -o test.fodt
+docker run --rm -v $(pwd):/documents/ curs/asciidoctor-od unoconv -f pdf test.fodt
 cd ../..
+
+#cd target/asciidoctor-pdf
+#asciidoctor -b fodt -T ../../lib/slim -r asciidoctor-mathematical -r asciidoctor-diagram chronicles-example.adoc -o pre.xml
+#ruby ../../lib/a-od-producer/asciidoctor-od.rb  -i pre.xml -f chronicles-template.fodt -o chronicles-example.fodt
+#docker run --rm -v $(pwd):/documents/ curs/asciidoctor-od unoconv -f pdf chronicles-example.fodt
+#cd ../..
 
